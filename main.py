8dfa68af417956a73c7fecd5b276373822e40256
JackFrost003 <96046315+JackFrost003@users.noreply.github.com>
2023-02-17 22:31:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,70 @@
-import telegram
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
 
-# Set up the Telegram bot using your bot's API token
-bot = telegram.Bot(token='5856420569:AAGqIr79LKzaxMcG6ElzyewDLDtfxjfRnOs')
+updater = Updater("5856420569:AAGqIr79LKzaxMcG6ElzyewDLDtfxjfRnOs",
+				use_context=True)
 
-# Define a function to send a message
-def send_message(chat_id, text):
-    bot.send_message(chat_id=chat_id, text=text)
 
-# Define a function to make a request to the Telegram API
-def make_request(method, params=None):
-    return bot.request(method, params)
+def start(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Hello sir, Welcome to the Bot.Please write\
+		/help to see the commands available.")
 
-# Example usage
-chat_id = 123456789 # Replace with the ID of the chat you want to send a message to
-message_text = "Hello, world!"
-send_message(chat_id, message_text)
+def help(update: Update, context: CallbackContext):
+	update.message.reply_text("""Available Commands :-
+	/youtube - To get the youtube URL
+	/linkedin - To get the LinkedIn profile URL
+	/gmail - To get gmail URL
+	/geeks - To get the GeeksforGeeks URL""")
 
-# Make a request to the Telegram API
-response = make_request("getUpdates")
-print(response)
+
+def gmail_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Your gmail link here (I am not\
+		giving mine one for security reasons)")
+
+
+def youtube_url(update: Update, context: CallbackContext):
+	update.message.reply_text("Youtube Link =>\
+	https://www.youtube.com/")
+
+
+def linkedIn_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"LinkedIn URL => \
+		https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
+
+
+def geeks_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"GeeksforGeeks URL => https://www.geeksforgeeks.org/")
+
+
+def unknown(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry '%s' is not a valid command" % update.message.text)
+
+
+def unknown_text(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry I can't recognize you , you said '%s'" % update.message.text)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+	Filters.command, unknown)) # Filters out unknown commands
+
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+
+updater.start_polling()
